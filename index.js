8e0ac34b3a3093891c86a9b30b00aca25ffdef71
gabbyprecious <gabbyprecious2000@gmail.com>
2022-04-23 19:19:45 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+function createCheckout(data){
+    url = "https://staging-api.flowertop.xyz/api/v1/checkout-inline?publicKey=" + data.publicKey + "&callbackUrl=" + data.callbackUrl + "&successUrl=" + data.successUrl + "&notificationEmail=" + data.notificationEmail + "&customerEmail=" + data.customerEmail + "&satoshis=" + data.satoshis + "&reference=" + data.reference
+    fetch(url, {
+        method: 'GET',
+        cache: 'no-cache', 
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+      }).then(function(res){
+        return res.json()
+      }).then(function(r){
+        console.log(r)
+        return r.data
+      }).then(setIframe)
+}
+
+function setIframe(checkout) {
+    console.log(checkout)
+    iframe = document.createElement("iframe")
+    url = "http://localhost:8080/" + checkout.id
+    iframe.src = url
+    iframe.setAttribute("width", window.innerWidth)
+    iframe.setAttribute("width", window.innerWidth)
+    iframe.setAttribute("height", window.innerHeight)
+    document.getElementById("button-pay").style.display = "none";
+    document.body.appendChild(iframe);
+}
+
+function removeIframe(iframe) {
+    document.body.removeChild(iframe);
+    document.getElementById("button-pay").style.display = "block";
+}
