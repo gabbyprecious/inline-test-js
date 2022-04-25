75450df77e567eeeccc96860edcb3fe7766fde30
gabbyprecious <gabbyprecious2000@gmail.com>
2022-04-25 16:13:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ function setIframe(checkout) {
     iframe = document.createElement("iframe")
     url = "http://localhost:8080/" + checkout.id
     iframe.src = url
+    setAttribute("allowtransparency", "true"),
     iframe.allow="clipboard-read; clipboard-write *"
-    iframe.style.cssText = "z-index: 999999999999999;background: transparent;background: rgba(0, 0, 0, 0.75);border: 0px none transparent;overflow-x: hidden;overflow-y: hidden;margin: 0;padding: 0;-webkit-tap-highlight-color: transparent;-webkit-touch-callout: none;position: fixed;left: 0;top: 0;width: 100%;height: 100%;transition: opacity 0.3s;-webkit-transition: opacity 0.3s;"
+    iframe.style.cssText = "z-index: 999999999999999;background: transparent;border: 0px none transparent;overflow-x: hidden;overflow-y: hidden;margin: 0;padding: 0;-webkit-tap-highlight-color: transparent;-webkit-touch-callout: none;position: fixed;left: 0;top: 0;width: 100%;height: 100%;visibility:hidden;"
     document.body.appendChild(iframe);
 }
 
