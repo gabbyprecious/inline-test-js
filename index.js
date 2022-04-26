b6419f3943fc600d77e4cd1be49fa454203becb2
gabbyprecious <gabbyprecious2000@gmail.com>
2022-04-26 21:00:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
+var checkoutData = null;
+
 function createCheckout(data){
-    url = "https://staging-api.flowertop.xyz/api/v1/checkout-inline?publicKey=" + data.publicKey + "&callbackUrl=" + data.callbackUrl + "&successUrl=" + data.successUrl + "&notificationEmail=" + data.notificationEmail + "&customerEmail=" + data.customerEmail + "&satoshis=" + data.satoshis + "&reference=" + data.reference
-    fetch(url, {
-        method: 'GET',
-        cache: 'no-cache', 
-        credentials: 'same-origin',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        redirect: 'follow',
-        referrerPolicy: 'no-referrer',
-      }).then(function(res){
-        return res.json()
-      }).then(function(r){
-        console.log(r)
-        return r.data
-      }).then(setIframe)
+  url = "https://staging-api.flowertop.xyz/api/v1/checkout-inline?publicKey=" + data.publicKey + "&callbackUrl=" + data.callbackUrl + "&successUrl=" + data.successUrl + "&notificationEmail=" + data.notificationEmail + "&customerEmail=" + data.customerEmail + "&satoshis=" + data.satoshis + "&reference=" + data.reference
+  fetch(url, {
+      method: 'GET',
+      cache: 'no-cache', 
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+    }).then(function(res){
+      return res.json()
+    }).then(function(r){
+      console.log(r)
+      return r.data
+    }).then(setIframe)
 }
 
 function setIframe(checkout) {
-    console.log(checkout)
-    iframe = document.createElement("iframe")
-    url = "http://localhost:8080/" + checkout.id
-    iframe.src = url
-    iframe.setAttribute("allowtransparency", "true"),
-    iframe.allow="clipboard-read; clipboard-write *"
-    iframe.style.cssText = "z-index: 999999999999999;background: transparent;border: 0px none transparent;overflow-x: hidden;overflow-y: hidden;margin: 0;padding: 0;-webkit-tap-highlight-color: transparent;-webkit-touch-callout: none;position: fixed;left: 0;top: 0;width: 100%;height: 100%;"
-    document.body.appendChild(iframe);
+  checkoutData = checkout;
+  iframe = document.createElement("iframe")
+  url = "https://staging-popup.flowertop.xyz/" + checkout.id
+  iframe.src = url
+  iframe.setAttribute("allowtransparency", "true"),
+  iframe.setAttribute("sandbox", "allow-scripts allow-same-origin")
+  iframe.allow="clipboard-read; clipboard-write *"
+  iframe.style.cssText = "z-index: 999999999999999;background: transparent;border: 0px none transparent;overflow-x: hidden;overflow-y: hidden;margin: 0;padding: 0;-webkit-tap-highlight-color: transparent;-webkit-touch-callout: none;position: fixed;left: 0;top: 0;width: 100%;height: 100%;"
+  document.body.appendChild(iframe);
 }
 
-function removeIframe(iframe) {
-    document.body.removeChild(iframe);
-    document.getElementById("button-pay").style.display = "block";
+function removeIframe() {
+  document.body.removeChild('iframe');
 }
+
+function callSuccessUrl(){
+  url = checkoutData.successUrl + "?checkoutId=" + checkoutData.id + "&reference=" + checkoutData.reference + "&status=" + 'paid'
+  window.location.replace(url);
+}
+
+window.callSuccessUrl = callSuccessUrl
 
 window.createCheckout = createCheckout
+
