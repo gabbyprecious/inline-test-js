ce54196f10682c560aabf0953549fa2dfe3b6732
gabbyprecious <gabbyprecious2000@gmail.com>
2022-04-28 17:41:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ function setIframe(checkout) {
   checkoutData = checkout;
   iframe = document.createElement("iframe")
   url = "https://staging-popup.flowertop.xyz/" + checkout.id
-//   url = "http://localhost:8080/" + checkout.id
   iframe.src = url
   iframe.setAttribute("allowtransparency", "true"),
   iframe.setAttribute("id", "checkout"),
