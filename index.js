882887ed224591986fdba43596af73427cca30c7
gabbyprecious <gabbyprecious2000@gmail.com>
2022-05-25 09:52:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,68 @@
 var checkoutData = null;
 
-function createCheckout(data) {
-  url = "https://staging-api.flowertop.xyz/api/v1/checkout-inline?publicKey=" + data.publicKey + "&callbackUrl=" + data.callbackUrl + "&successUrl=" + data.successUrl + "&notificationEmail=" + data.notificationEmail + "&customerEmail=" + data.customerEmail + "&satoshis=" + data.satoshis + "&reference=" + data.reference
+function getUrlBase(environment) {
+  return environment == "production" ? "https://api.bitnob.co/" : "https://staging-api.flowertop.xyz/";
+}
+
+function buildUrl(urlBase, data){
+  return  url = urlBase + 'api/v1/checkout-inline?publicKey=' +
+  data.publicKey +
+  "&callbackUrl=" +
+  data.callbackUrl +
+  "&successUrl=" +
+  data.successUrl +
+  "&notificationEmail=" +
+  data.notificationEmail +
+  "&customerEmail=" +
+  data.customerEmail +
+  "&satoshis=" +
+  data.satoshis +
+  "&reference=" +
+  data.reference;
+}
+
+function buildFetchData({ method, data }) {
+var fetchData = {
+  method,
+  cache: "no-cache",
+  credentials: "same-origin",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  redirect: "follow",
+  referrerPolicy: "no-referrer",
+};
+if (data) fetchData.body = JSON.stringify(data);
+return fetchData;
+}
+
+function convertToSatoshis(amount, currency, environment) {
+  conversion = `${currency.toUpperCase()}_SAT`;
+
+  let url = getUrlBase(environment) + "api/v1/rates/convert-currency/";
+
+  data = { conversion: conversion, amount: amount };
+  conversion = `${currency.toUpperCase()}_SAT`;
+  return fetch(url, buildFetchData({ data, method: "POST" }));
+}
+
+function createCheckout(data, environment) {
   createLoaderIframe();
-  fetch(url, {
-    method: 'GET',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-  }).then(function (res) {
-    return res.json()
-  }).then(function (r) {
-    console.log(r)
-    return r.data
-  }).then(setIframe)
+
+  fetch(buildUrl(getUrlBase(environment), data), buildFetchData({method: "GET" })).then( (res) => 
+    res.json()
+  ).then((response) =>
+    setIframe(response.data, environment)
+  )
+}
+
+function initializePayment(data, environment) {
+  convertToSatoshis(data.amount, data.currency, environment).then( (res) => 
+    res.json()
+  ).then( (response) => {
+    data.satoshis = response.data;
+    createCheckout(data, environment);
+});
 }
 
 function removeIframe(id) {
@@ -26,52 +71,67 @@ function removeIframe(id) {
 }
 
 function createLoaderIframe() {
-  iframe = document.createElement("iframe")
+  iframe = document.createElement("iframe");
   iframe.setAttribute("allowtransparency", "true"),
-  iframe.setAttribute("id", "loader")
-  iframe.src = "data:text/html;charset=utf-8, <body style='background-color:rgba(0, 0, 0, 0.8);'>" +
+    iframe.setAttribute("id", "loader");
+  iframe.src =
+    "data:text/html;charset=utf-8, <body style='background-color:rgba(0, 0, 0, 0.8);'>" +
     "<div style='position: absolute;display: flex;flex-direction: column;align-items: center;justify-content: center;height: 100%;width: 100%;z-index: 1000; text-align:center'>" +
     "<img src='https://res.cloudinary.com/gabbyprecious/image/upload/v1651003230/haxqforaozaqsl0ksber.gif'width='100px' alt='Logo'/>" +
-    "</div></body>"
-  iframe.style.cssText = "z-index: 999999999999999;background: transparent;border: 0px none transparent;overflow-x: hidden;overflow-y: hidden;margin: 0;padding: 0;-webkit-tap-highlight-color: transparent;-webkit-touch-callout: none;position: fixed;left: 0;top: 0;width: 100%;height: 100%;"
+    "</div></body>";
+  iframe.style.cssText =
+    "z-index: 999999999999999;background: transparent;border: 0px none transparent;overflow-x: hidden;overflow-y: hidden;margin: 0;padding: 0;-webkit-tap-highlight-color: transparent;-webkit-touch-callout: none;position: fixed;left: 0;top: 0;width: 100%;height: 100%;";
   document.body.appendChild(iframe);
 }
 
-function setIframe(checkout) {
+function setIframe(checkout, environment) {
   checkoutData = checkout;
-  iframe = document.createElement("iframe")
-  url = "https://staging-popup.flowertop.xyz/" + checkout.id
-  iframe.src = url
+  iframe = document.createElement("iframe");
+  if (environment == "sandbox") {
+    url = "https://staging-popup.flowertop.xyz/" + checkoutData.id;
+  }
+  if (environment == "production") {
+    url = "https://popup.bitnob.co/" + checkoutData.id;
+  }
+  iframe.src = url;
   iframe.setAttribute("allowtransparency", "true"),
-  iframe.setAttribute("id", "checkout"),
-  iframe.setAttribute("sandbox", "allow-scripts allow-same-origin")
-  iframe.style.cssText = "z-index: 999999999999999;background: transparent;border: 0px none transparent;overflow-x: hidden;overflow-y: hidden;margin: 0;padding: 0;-webkit-tap-highlight-color: transparent;-webkit-touch-callout: none;position: fixed;left: 0;top: 0;width: 100%;height: 100%;"
+    iframe.setAttribute("id", "checkout"),
+    iframe.setAttribute("sandbox", "allow-scripts allow-same-origin");
+  iframe.style.cssText =
+    "z-index: 999999999999999;background: transparent;border: 0px none transparent;overflow-x: hidden;overflow-y: hidden;margin: 0;padding: 0;-webkit-tap-highlight-color: transparent;-webkit-touch-callout: none;position: fixed;left: 0;top: 0;width: 100%;height: 100%;";
   document.body.appendChild(iframe);
 }
 
 function callSuccessUrl() {
-  url = checkoutData.successUrl + "?checkoutId=" + checkoutData.id + "&reference=" + checkoutData.reference + "&status=" + 'paid'
+  url =
+    checkoutData.successUrl +
+    "?checkoutId=" +
+    checkoutData.id +
+    "&reference=" +
+    checkoutData.reference +
+    "&status=" +
+    "paid";
   window.location.replace(url);
 }
 
 window.onmessage = function (e) {
-  if (e.data == 'mounting-loader') {
-    removeIframe('loader');
+  if (e.data == "mounting-loader") {
+    removeIframe("loader");
   }
-  if (e.data == 'popup-closed') {
-    removeIframe('checkout');
+  if (e.data == "popup-closed") {
+    removeIframe("checkout");
   }
-  if (e.data == 'paid') {
+  if (e.data == "paid") {
     setTimeout(() => {
       window.callSuccessUrl();
     }, 10000);
-    
   }
-}
+};
 
-window.callSuccessUrl = callSuccessUrl
+window.callSuccessUrl = callSuccessUrl;
 
-window.createCheckout = createCheckout
+window.createCheckout = createCheckout;
 
-window.checkoutData = checkoutData
+window.initializePayment = initializePayment;
 
+window.checkoutData = checkoutData;
